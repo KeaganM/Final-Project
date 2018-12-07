104883c6abcf3d8f943e040ea2389fc2cee5260d
Keagan McNew <44347637+KeaganM@users.noreply.github.com>
2018-12-06 18:10:47 -0600

    Delete loader.js

--- main/final/loader.js
+++
@@ -1,9 +0,0 @@
-var myVar;
-function myFunction() {
-    myVar = setTimeout(showPage, 3000);
-    document.getElementById("masterid").style.opacity = 1;
-}
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
-}
